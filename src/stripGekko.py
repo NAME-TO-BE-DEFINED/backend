4a705bb1c101532964b6867f9649281e39d53e87
AaravAtGit <aarav54897@gmail.com>
2024-05-19 02:55:33 +0530

    Added everything in 1 commit | It's ugly

--- src/stripGekko.py
+++ src/stripGekko.py
@@ -1,6 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+from binance import Client
+import os
+from dotenv import load_dotenv
+import pprint
 
+
+load_dotenv()
 
 class StripGekko:
 
@@ -8,6 +14,9 @@ class StripGekko:
         # URL's for scraping data
         self.TVL = "https://www.coingecko.com/en/chains"
         self.COIN = "https://www.coingecko.com/en/coins/"
+        self.api_key = os.getenv("API_KEY")
+        self.secret_key = os.getenv("secret_key")
+        self.client = Client(self.api_key, self.secret_key)
     
 
     def getTVL(self):
@@ -32,6 +41,56 @@ class StripGekko:
 
         return data
 
+    # Function too ugly dont peak
+    def getCoin(self, coin):
+        r = requests.get(self.COIN + coin)
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+
+        price = soup.find("span", {"data-price-target":"price"}).text
+        movement = soup.find("span", {"data-percent-change-target":"percent"})
+        symbol = soup.find("span", {"data-view-component":"true", "class":"tw-font-normal tw-text-gray-500 dark:tw-text-moon-200 tw-text-sm tw-leading-5 tw-mt-0.5"}).text.replace("Price", "").strip() # super long line go brrr
         
-    
+        # adds - if down
+        movement_binary = movement.get("class")
+        movement = movement.text
+
+        if movement_binary[0] == "gecko-down":
+            movement = "-" + movement
+        
+
+        data = {
+            "price": price,
+            "movement": movement,
+            "symbol": symbol
+        }
+
+
+        # Get data(Yea very detailed comment)
+        tb = soup.find_all("tbody")[1]
+
+        for row in tb.find_all("tr"):
+            cols = row.find_all(['th', 'td'])
+
+            # Needed to split lines to filter data
+            x = cols[0].text.splitlines()[1].strip() 
+            y = cols[1].text.splitlines()[1].strip()
+            data[x] = y
+
+        
+        
+
+
+
+
+        return data
+
+    def getHistoricalData(self, coinpair):
+        print(coinpair)
+        klines = self.client.get_historical_klines(coinpair, Client.KLINE_INTERVAL_1HOUR, "1 day ago")
+        return klines   
+
+
+
+
 
